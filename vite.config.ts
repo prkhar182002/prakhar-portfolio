c0a65169f9c15f7d0608207200c4a8fab92d8c27
prkhar182002 <70217453+prkhar182002@users.noreply.github.com>
2025-12-03 18:45:33 +0530

    Fix base path and hostname in Vite config
    Updated base path and hostname for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,32 @@ import sitemap from 'vite-plugin-sitemap';
 import { robots } from 'vite-plugin-robots';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',   // FIXED ❗
+
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: true,
-    // terserOptions:
     chunkSizeWarningLimit: 1600,
   },
+
   plugins: [
     tailwindcss(),
     vue(),
     robots(),
     sitemap({
-      hostname: 'https://hetari.github.io/',
-      basePath: 'portfolio',
-      changefreq: 'hourly', // default: 'daily'
+      hostname: 'https://prakhar.devcorebit.com/',   // FIXED ❗
+      basePath: '/',                                 // FIXED ❗
+      changefreq: 'hourly',
       priority: 1,
     }),
   ],
+
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+    alias: { '@': '/src' },
   },
-  server: {
-    // watch: {
-    //   usePolling: true,
-    //   interval: 1000,
-    //   ignored: ['!**/src/**/*.{js,ts,jsx,tsx}'],
-    // },
-  },
+
   optimizeDeps: {
     exclude: ['@tailwindcss/vite'],
     force: true,
